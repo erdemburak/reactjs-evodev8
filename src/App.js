755b4083664f7a8d7333337fe1b9540b4c3950ee
erdemburak <burakerdem6@hotmail.com>
2023-10-18 22:11:06 +0300

    ev odev 8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+import NotFound from "./pages/NotFound"
+import UseEffectOdev7 from "./pages/UseEffectOdev7"
+import OrderPage from "./pages/OrderPage"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  return (<>
+
+    <ul style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+      <li><Link to='/contact'>Contact</Link></li>
+      <li><Link to='/customers'>Customers</Link></li>
+      <li><Link to='/orderpage'>Orders</Link></li>
+      <li><Link to='/suppliersodev'>Suppliers Odev</Link></li>
+    </ul>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/orderpage" element={<OrderPage />} />
+      <Route path="/suppliersodev" element={<UseEffectOdev7 />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </>)
+
 }
 
-export default App;
+export default App
---
+++ src/pages/UseEffectOdev7.jsx
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+function UseEffectOdev7() {
+
+    const [suppliers, setsuppliers] = useState([]);
+
+    useEffect(() => {
+        loadSuppliers();
+    }, [])
+
+    const loadSuppliers = () => {
+        axios.get('https://northwind.vercel.app/api/suppliers')
+            .then(res => {
+                setsuppliers(res.data);
+            })
+    }
+
+    const deleteSupplier = (id) => {
+
+        var result = window.confirm("Want to delete?");
+        if (result) {
+
+            axios.delete('https://northwind.vercel.app/api/suppliers/' + id)
+                .then(res => {
+                    loadSuppliers();
+                })
+        }
+    }
+
+    return (<>
+        <div style={{ padding: '2%' }}>
+
+            <table className='w3-table w3-striped w3-bordered w3-hoverable'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Company Name</th>
+                        <th>Contact Name</th>
+                        <th>Contact Title</th>
+                        <th>Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        suppliers && suppliers.map(item => {
+                            return <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.companyName}</td>
+                                <td>{item.contactName}</td>
+                                <td>{item.contactTitle}</td>
+                                <td>{item.address.country}</td>
+                                <td><button onClick={() => deleteSupplier(item.id)}>Delete</button></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </>
+    )
+}
+
+export default UseEffectOdev7
